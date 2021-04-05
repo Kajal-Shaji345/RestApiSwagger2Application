6a4e01b697ce405e6d955ceaa6a139b4d1cf4179
Kajal-Shaji345 <kajalshaji345@gmail.com>
2021-04-05 01:03:54 -0700

    Initial commit

---
+++ src/test/java/com/spingbootjsf/RestApiSwagger2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spingbootjsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSwagger2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
